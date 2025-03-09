78bc7bb45207f119ac3d8b29b151cecd1630af39
mgaudin <matthieugaudin917@gmail.com>
2025-03-09 16:26:54 +0100

    feat(parser): gets the variable name and value from envp

--- srcs/parser/expansion.c
+++ srcs/parser/expansion.c
@@ -30,51 +30,41 @@ char	*get_var_name(char *env_var)
 	return (var_name);
 }
 
-int		get_var_len(char *str, int start)
+char	*get_var_value(char **envp, char *var_name)
 {
-	int	i;
+	char	*var_value;
+	char	*envp_name;
+	int		i;
 
 	i = 0;
-	while (is_posix_std(str[start + i]))
-	{
-		i++;
-	}
-	return (i);
-}
-
-char	*get_env_var(char *str, char **envp)
-{
-	char	*envp_name;
-	char	*str_name;
-
+	var_value = NULL;
+	envp_name = NULL;
 	while (*envp)
 	{
 		envp_name = get_var_name(*envp);
-		str_name = get_var_name(str);
-		if (ft_strcmp(envp_name, str_name) == 0)
-			return (*envp);
+		if (ft_strcmp(envp_name, var_name) == 0)
+		{
+			while (is_posix_std(*((*envp) + i)))
+				i++;
+			var_value = ((*envp) + i + 1);
+			break ;
+		}
 		envp++;
 	}
-	free(envp_name);
-	free(str_name);
-	return (NULL);
+	return (var_value);
 }
 
 void	expand_var(t_token *node, char **envp, int start)
 {
-	char	*env_var;
-	int		env_len;
-	// char	*tmp_var;
+	char	*var_value;
+	char	*var_name;
 	int		remainder;
 	int		len;
 
-	env_var = get_env_var(node->value + start, envp);
-	env_len = 0;
-	if (env_var != NULL)
-		env_len = ft_strlen(env_var);
-	remainder = ft_strlen(node->value + start + ft_strlen(node->value + get_var_len(node->value, start)));
-	len = (start - 1) + env_len + remainder;
-	// tmp_var = malloc(sizeof(char) * (len + 1));
+	var_name = get_var_name(node->value + start);
+	var_value = get_var_value(envp, var_name);
+	remainder = ft_strlen(node->value + start + ft_strlen(var_name));
+	len = (start - 1) + ft_strlen(var_value) + remainder;
 	printf("%d\n", len);
 }
 
@@ -111,6 +101,6 @@ int main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 
-	head = tokenizer("ok$USE'R'ok");
+	head = tokenizer("ok\"$USERR\"ok");
 	expansion(head, envp);
 }
