ce7656f5bcb862ee4ab241d281cdc57041d53593
Doooriian <dorian.ley91@gmail.com>
2025-03-10 12:51:12 +0100

    feat(env): implement environment variable parsing and linked list structure

--- includes/minishell.h
+++ includes/minishell.h
@@ -9,4 +9,11 @@
 # include <readline/history.h>
 # include "../libs/libft/libft.h"
 
+typedef struct s_env
+{
+	char			*name;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
 #endif /* MINISHELL_H */
