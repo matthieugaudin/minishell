a019dbd7240275164e3bcb8aeeb85d240038f4f7
Doooriian <dorian.ley91@gmail.com>
2025-03-11 09:58:50 +0100

    fix(parser): remove merge conflict remnants in env.c

--- srcs/parser/env.c
+++ srcs/parser/env.c
@@ -1,10 +1,4 @@
 #include "../../includes/minishell.h"
-<<<<<<< HEAD
-
-bool is_posix_std(char c)
-{
-	if (ftisalnum(c) || c == '_')
-=======
 #include "../../includes/tokenizer.h"
 
 /*
@@ -15,52 +9,11 @@ or an underscore.
 bool	is_posix_std(char c)
 {
 	if (ft_isalnum(c) || c == '_')
->>>>>>> main
 		return (true);
 	else
 		return (false);
 }
 
-<<<<<<< HEAD
-char *get_var_name(char *env_var)
-{
-	char *var_name;
-	int i;
-
-	i = 0;
-	while (is_posix_std(env_var[i]))
-		i++;
-	var_name = malloc(sizeof(char) * (i + 1));
-	ft_strlcpy(var_name, env_var, i + 1);
-	return (var_name);
-}
-
-void append_env_node(t_env **env, char *str)
-{
-	t_env *node;
-
-	node = malloc(sizeof(t_env));
-	if (*env == NULL)
-		*env = node;
-	node->name = get_var_name(str);
-	node->value = get_var_value(str);
-	// set the prev node next to the new one
-	node->next = NULL;
-}
-
-void parse_envp(char **envp)
-{
-	size_t i;
-	t_env *env;
-
-	i = 0;
-	env = NULL;
-	while (envp[i])
-	{
-		append_env_node(&env, envp[i]);
-		i++;
-	}
-=======
 /*
 Takes an environment variable from envp and returns its name.
 */
@@ -126,5 +79,4 @@ t_env   *create_env(char **envp)
 		envp++;
 	}
 	return (env);
->>>>>>> main
 }
