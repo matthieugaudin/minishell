a6fd3298cd609a155faab51e1020efe80a3b296f
Doooriian <dorian.ley91@gmail.com>
2025-03-11 17:13:41 +0100

    feat(builtins): implement ft_check_and_remove function in ft_unset.c

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -1,12 +1,21 @@
 #include "../../includes/minishell.h"
 
+void	ft_check_and_remove(char *str)
+{
+	
+}
+
 int	ft_unset(char **args)
 {
 	size_t	i;
 
 	i = 0;
-	while (args[i] && is_posix_std(args[i]))
+	while (args[i])
 	{
-
+		if (is_posix_std(args[i]))
+		{
+			ft_check_and_remove(args[i]);
+		}
+		i++;
 	}
 }
