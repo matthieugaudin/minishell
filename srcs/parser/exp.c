e6e95c0b07684057137c22afe4cc136420f17c08
Doooriian <dorian.ley91@gmail.com>
2025-03-13 11:48:43 +0100

    refactor(parser): simplify node insertion and improve create_export function

--- srcs/parser/exp.c
+++ srcs/parser/exp.c
@@ -1,21 +1,10 @@
 #include "../../includes/minishell.h"
 
-static t_env	*ft_last_node(t_env *exp)
-{
-	if (!exp)
-		return (NULL);
-	while (exp->next != NULL)
-	{
-		exp = exp->next;
-	}
-	return (exp);
-}
-
 static t_env	*ft_new_node(char *name, char *value)
 {
 	t_env	*new;
 
-	new = (t_list *)malloc(sizeof(t_list));
+	new = (t_env *)malloc(sizeof(t_env));
 	if (!new)
 		return (NULL);
 	new->name = name;
@@ -24,41 +13,45 @@ static t_env	*ft_new_node(char *name, char *value)
 	return (new);
 }
 
-static void	ft_add_back(t_env **exp, t_env *new)
+void	ft_insert_node(t_env **exp, t_env *new_node)
 {
-	t_env	*last;
+	t_env	*current;
 
-	if (!exp)
-		return ;
-	if (!*exp)
+	if (!exp || !new_node)
+		return;
+	if (*exp == NULL || strcmp(new_node->name, (*exp)->name) < 0)
 	{
-		*exp = new;
-		return ;
+		new_node->next = *exp;
+		*exp = new_node;
+		return;
 	}
-	last = ft_lstlast(*exp);
-	last->next = new;
+	current = *exp;
+	while (current->next && strcmp(new_node->name, current->next->name) > 0)
+	{
+		current = current->next;
+	}
+	new_node->next = current->next;
+	current->next = new_node;
 }
 
 void	create_export(void)
 {
+	t_env	*exp;
 	t_env	*cur_env;
-	t_env	*cur_exp;
+	t_env	*new_node;
 	t_env	**env;
-	t_env	**exp;
 
 	env = ft_getter("env");
+	if (!env)
+		return ;
 	cur_env = *env;
-	cur_exp = *exp;
-	ft_add_back(exp, ft_new_node(cur_env->name, cur_env->value));
-	while (cur_env->next)
+	while (cur_env)
 	{
+		new_node = ft_new_node(cur_env->name, cur_env->value);
+		if (new_node)
+			return ;
+		ft_insert_node(&exp, new_node);
 		cur_env = cur_env->next;
-		while (cur_exp)
-		{
-			if (ft_strcmp(cur_exp->name, cur_env->name) > 0)
-				ft_add_back(exp, ft_new_node(cur_env->name, cur_env->value));
-			cur_exp = cur_exp->next;
-		}
 	}
-	ft_setter(exp, "exp");
+	ft_setter(&exp, "exp");
 }
