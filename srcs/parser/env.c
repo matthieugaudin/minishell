1e54e3be03540e16c625fff1c7061787a0c1cc8e
Doooriian <dorian.ley91@gmail.com>
2025-03-14 15:19:50 +0100

    refactor(parser): remove unused get_env_value function and adjust append_env_node visibility

--- srcs/parser/env.c
+++ srcs/parser/env.c
@@ -29,29 +29,7 @@ static char	*get_env_name(char *envp_str)
 	return (env_name);
 }
 
-<<<<<<< HEAD
-char	*get_env_value(char *name)
-{
-	char	*env_value;
-	int		i;
-	int		j;
-
-	i = 0;
-	while (is_posix_std(name[i]))
-		i++;
-	i++;
-	j = 0;
-	while (name[i + j])
-		j++;
-	env_value = malloc(sizeof(char) * (j + 1));
-	ft_strlcpy(env_value, name + i, j + 1);
-	return (env_value);
-}
-
-void	append_env_node(t_env **env, char *envp)
-=======
 static void	append_env_node(t_env **env, char *envp)
->>>>>>> doley
 {
 	t_env	*last_node;
 	t_env	*node;
