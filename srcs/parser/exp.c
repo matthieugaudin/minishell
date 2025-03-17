0c988bb2c1f895eae02b0b73c58d0eb423c2f689
Doooriian <dorian.ley91@gmail.com>
2025-03-17 12:32:48 +0100

    refactor(parser): remove commented-out code and improve code clarity in exp.c

--- srcs/parser/exp.c
+++ srcs/parser/exp.c
@@ -53,75 +53,16 @@ void	create_export(t_data *data)
 	}
 }
 
+
+
+
+
+
+
+
+// #include "../../includes/minishell.h"
+
 // int	count_list_nodes(t_env *list)
-// {
-//     int		count;
-//     t_env	*tmp;
-
-//     count = 0;
-//     tmp = list;
-//     while (tmp)
-//     {
-//         count++;
-//         tmp = tmp->next;
-//     }
-//     return (count);
-// }
-
-// void	compare_lists(char **envp, t_data *data)
-// {
-//     int		system_count = 0;
-//     int		env_count = count_list_nodes(data->env);
-//     int		exp_count = count_list_nodes(data->exp);
-//     char	**system_vars = envp;
-
-//     // Comptage des variables système
-//     while (*system_vars)
-//     {
-//         system_count++;
-//         system_vars++;
-//     }
-
-//     // Vérification comptage de base
-//     printf("\n=== COMPARAISON ===\n");
-//     printf("Variables système : %d\n", system_count);
-//     printf("Variables ENV     : %d\n", env_count);
-//     printf("Variables EXPORT  : %d\n", exp_count);
-
-//     // Vérification contenu ENV
-//     int		missing = 0;
-//     int		diff_value = 0;
-//     char	**envp_ptr = envp;
-
-//     while (*envp_ptr)
-//     {
-//         char	*eq = strchr(*envp_ptr, '=');
-//         char	*true_name = strndup(*envp_ptr, eq - *envp_ptr);
-//         char	*true_value = eq + 1;
-//         t_env	*node = data->env;
-//         bool	found = false;
-
-//         while (node)
-//         {
-//             if (strcmp(true_name, node->name) == 0)
-//             {
-//                 found = true;
-//                 if (strcmp(true_value, node->value) != 0)
-//                     diff_value++;
-//                 break ;
-//             }
-//             node = node->next;
-//         }
-//         if (!found && strcmp(true_name, "_") != 0)
-//             missing++;
-//         free(true_name);
-//         envp_ptr++;
-//     }
-
-//     // Vérification tri EXPORT
-//     int		sorted = true;
-//     t_env	*current = data->exp;
-//     while (current && current->next)int	count_list_nodes(t_env *list)
 // {
 //     int		count;
 //     t_env	*tmp;
@@ -231,32 +172,4 @@ void	create_export(t_data *data)
 
 //     return (0);
 // }
-//         printf("\n\x1b[31mPROBLÈMES DÉTECTÉS\x1b[0m\n");
-// }
-
-// int	main(int argc, char **argv, char **envp)
-// {
-//     t_data	*data = malloc(sizeof(t_data));
-
-//     (void)argc;
-//     (void)argv;
-//     create_env(data, envp);
-
-//     printf("=== VARIABLES SYSTÈME ===\n");
-//     for (char **e = envp; *e; e++)
-//         printf("%s\n", *e);
-
-//     printf("\n=== VARIABLES ENV ===\n");
-//     for (t_env *e = data->env; e; e = e->next)
-//         printf("%s=%s\n", e->name, e->value);
-
-//     printf("\n=== VARIABLES EXPORT ===\n");
-//     for (t_env *e = data->exp; e; e = e->next)
-//         printf("%s=%s\n", e->name, e->value);
-
-//     compare_lists(envp, data);
-
-//     return (0);
-// }
-
 
