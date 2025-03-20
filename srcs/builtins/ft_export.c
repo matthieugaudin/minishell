b23c69a045b5f91e8bf6f0b9992f0f5d5f7a7ca8
Doooriian <dorian.ley91@gmail.com>
2025-03-20 15:39:29 +0100

    refactor(export): simplify ft_export and improve error handling in ft_find_egal_pos

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -4,23 +4,16 @@ static int	ft_find_egal_pos(char *str)
 {
 	size_t	i;
 
+	if (!str || !*str || ft_isdigit(str[0]))
+		return (-1);
 	i = 0;
-	while (str[i])
+	while (str[i] && str[i] != '=')
 	{
-		if (!is_posix_std(str[i]) && str[i] != '=')
+		if (!is_posix_std(str[i]))
 			return (-1);
-		if (str[i] == '=')
-			return (i);
 		i++;
 	}
-	return (0);
-}
-
-static int	ft_handle_export_error(char *arg)
-{
-	ft_putstr_fd("export: not a valid identifier: ", 2);
-	ft_putendl_fd(arg, 2);
-	return (1);
+	return (i);
 }
 
 static void ft_update_env_node(t_env **env, t_env *new_node)
@@ -52,21 +45,17 @@ static void ft_update_env_node(t_env **env, t_env *new_node)
 	return ;
 }
 
-static int ft_export_with_value(t_data *data, char *arg, size_t i_egal)
+static int ft_update_env_exp(t_data *data, char *arg, size_t i_egal)
 {
 	char	*name;
 	char	*value;
 
+	if (!name || !value)
+		return (1);
 	name = ft_substr(arg, 0, i_egal);
 	value = ft_substr(arg, i_egal + 1, ft_strlen(arg) - i_egal - 1);
-	if (!name || !value)
-	{
-		free(name);
-		free(value);
-		return (1);
-	}
-	ft_update_env_node(&data->env, ft_new_node(name, value));
 	ft_insert_exp_node(&data->exp, ft_new_node(name, value));
+	ft_update_env_node(&data->env, ft_new_node(name, value));
 	free(name);
 	free(value);
 	return (0);
@@ -76,23 +65,15 @@ int ft_export(t_data *data, char **args)
 {
 	size_t	i;
 	size_t	i_egal;
-	int		status;
 
 	i = 0;
-	status = 0;
 	while (args[i])
 	{
 		i_egal = ft_find_egal_pos(args[i]);
 		if (i_egal == 1)
 			ft_insert_exp_node(&data->exp, ft_new_node(args[i], NULL));
 		else if (i_egal)
-		{
-			if (ft_export_with_value(data, args[i], i_egal) != 0)
-				status = 1;
-		}
-		else
-			status = ft_handle_export_error(args[i]);
-		i++;
+			ft_update_env_exp(data, args[i], i_egal);
 	}
-	return (status);
+	return (0);
 }
