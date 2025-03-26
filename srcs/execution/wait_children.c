d21c8145f6904968752c37e82794955d87116d48
mgaudin <matthieugaudin917@gmail.com>
2025-03-26 19:20:09 +0100

    fix(execution): wait all children wihtout zombie processes

--- srcs/execution/wait_children.c
+++ srcs/execution/wait_children.c
@@ -3,12 +3,18 @@
 void	wait_children(t_data *data, pid_t last_pid)
 {
 	int		status;
+	t_cmd	*cmds;
 
-	if (wait(&status) == last_pid)
+	cmds = data->cmds;
+	while (cmds)
 	{
-		if (WIFEXITED(status))
-			data->status = WEXITSTATUS(status);
-		else if (WIFSIGNALED(status))
-			data->status = (128 + WTERMSIG(status));
+		if (wait(&status) == last_pid)
+		{
+			if (WIFEXITED(status))
+				data->status = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				data->status = (128 + WTERMSIG(status));
+		}
+		cmds = cmds->next;
 	}
 }
