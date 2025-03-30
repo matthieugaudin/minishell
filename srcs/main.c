5f5fbf2d741f5fa253366685a7361a95f143fe6a
mgaudin <matthieugaudin917@gmail.com>
2025-03-30 22:16:17 +0200

    update readline algorithm

--- srcs/main.c
+++ srcs/main.c
@@ -3,18 +3,35 @@
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
+	t_data	*data;
+	t_token *token;
+	t_token *tmp;
 
 	(void)argc;
 	(void)argv;
-	(void)envp;
-	printf("\'");
-	while (1)
+	data = malloc(sizeof(t_data));
+	data->env = create_env(envp);
+	data->exp = create_export(data->env);
+	while (true)
 	{
 		rl_outstream = stderr;
-		line = readline("minishell>");
-		if (line)
+		line = readline("minishell> ");
+		if (line[0])
+		{
+			token = tokenize_line(line);
+			parse_tokens(token);
+			expand_tokens(token, data->env);
+			remove_quotes(token);
+			printf("%s\n\n", line);
+			tmp = token;
+			while (token)
+			{
+				printf("%s %d\n", token->value, token->type);
+				token = token->next;
+			}
+			free_tokens(tmp, false);
 			add_history(line);
-		free(line);
+		}
 	}
 	rl_clear_history();
 	return (0);
