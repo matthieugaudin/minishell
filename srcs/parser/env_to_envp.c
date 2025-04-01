78a4057708cf7cba63bb4ab26677850dfa45bbfd
Doooriian <dorian.ley91@gmail.com>
2025-04-01 17:17:18 +0200

    feat(parser): enhance environment variable handling and memory management

--- srcs/parser/env_to_envp.c
+++ srcs/parser/env_to_envp.c
@@ -1,5 +1,20 @@
 #include "../includes/parser.h"
 
+static void	free_envp(char **envp)
+{
+	int	i;
+
+	i = 0;
+	if (!envp)
+		return;
+	while (envp[i])
+	{
+		free(envp[i]);
+		i++;
+	}
+	free(envp);
+}
+
 static int	find_env_size(t_env *env)
 {
 	int	i;
@@ -13,19 +28,46 @@ static int	find_env_size(t_env *env)
 	return (i);
 }
 
+static char	*assign_envp_var(t_env	*current)
+{
+	char	*tmp;
+	char	*str;
+
+	tmp = ft_strjoin(current->name, "=");
+	if (!tmp)
+		return (NULL);
+	str = ft_strjoin(tmp, current->value);
+	free(tmp);
+	if (!str)
+		return (NULL);
+	return (str);
+}
+
 char	**convert_env_to_envp(t_env *env)
 {
 	int		i;
 	int		env_size;
 	char	**envp;
+	t_env	*current;
 
 	env_size = find_env_size(env);
-	envp = malloc(sizeof(char *) * env_size + 1);
+	envp = malloc(sizeof(char *) * (env_size + 1));
 	if (!envp)
 		return (NULL);
-	i = 0;
-	while (i < env_size)
+	i = -1;
+	current = env;
+	while (current && ++i < env_size)
 	{
-		
+		envp[i] = assign_envp_var(current);
+		if (!envp[i])
+		{
+			while (--i >= 0)
+				free(envp[i]);
+			free(envp);
+			return (NULL);
+		}
+		current = current->next;
 	}
+	envp[++i] = NULL;
+	return (envp);
 }
