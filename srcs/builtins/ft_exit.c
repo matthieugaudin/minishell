10e8e4e69ad775aa635c4f4f6cdbf197944741e7
Matthieu Gaudin <mgaudin@1F3.42angouleme.fr>
2025-04-03 13:32:45 +0200

    feat(builtins): exit builtin done

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -1,6 +1,89 @@
 #include "../../includes/builtins.h"
 
-void    ft_exit(void)
+static bool	threshold_exceeded(char *str, int res, int sign, int i)
 {
-    exit(1);
+	if (((-res == (LLONG_MIN / 10) && str[i] > '8')
+		|| (-res < (LLONG_MIN / 10) && str[i]))
+		|| (sign != -1 && ((res == (LLONG_MAX / 10)
+		&& str[i] > '7') || (res > (LLONG_MAX / 10) && str[i]))))
+		return (true);
+	else
+		return (false);
 }
+
+static bool	is_overflow(char *str)
+{
+	long long	i;
+	long long	res;
+	int			sign;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] == 32 || (9 <= str[i] && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		if (threshold_exceeded(str, res, sign, i))
+			return (true);
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (false);
+}
+
+static bool	is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(*str) && !(*str == '-' && *(str + 1)))
+			return (false);
+		i++;
+	}
+	if (is_overflow(str))
+		return (false);
+	return (true);
+}
+
+void    ft_exit(char **args)
+{
+
+	if (!args[0])
+	{
+		ft_putendl_fd("exit", 1);
+		exit(exit_code(0, false));
+	}
+	else if (!is_numeric(args[0]))
+	{
+		ft_putendl_fd("exit", 1);
+		ft_putstr_fd("minishell: exit: ", 2);
+		ft_putstr_fd(args[0], 2);
+		ft_putendl_fd(": numeric argument required", 2 );
+		exit(2);
+	}
+	else if (args[1])
+	{
+		ft_putendl_fd("exit", 1);
+		ft_putendl_fd("minishell: exit: too many arguments", 2);
+		exit_code(1, true);
+	}
+	else
+	{
+		ft_putendl_fd("exit", 1);
+		exit(ft_atoi(args[0]));
+	}
+}
+
+// int main(void)
+// {
+// 	printf("%d", is_overflow("-9223372036854775808"));
+// }
