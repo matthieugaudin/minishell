8104b111e3ed99fce2c4b26fbb28ca58f982c7e1
mgaudin <mgaudin@student.42angouleme.fr>
2025-04-09 18:24:27 +0200

    fix: no leaks or segfault on pipe syscall fail

--- srcs/execution/create_pipes.c
+++ srcs/execution/create_pipes.c
@@ -1,5 +1,62 @@
 #include "../../includes/execution.h"
 
+static void	close_failure(t_cmd *cmds, int **pipes, int j)
+{
+	int i;
+
+	i = 0;
+	while (i < j)
+	{
+		close(pipes[cmds->index][0]);
+		close(pipes[cmds->index][1]);
+		cmds = cmds->next;
+		i++;
+	}
+}
+
+static void	free_data_pipe(t_data *data, int j)
+{
+	t_cmd	*tmp_cmd;
+	int		i;
+
+	i = 0;
+	while (data->cmds)
+	{
+		if (data->cmds->fd_in != 0)
+			close(data->cmds->fd_in);
+		if (data->cmds->fd_out != 1)
+			close(data->cmds->fd_out);
+		free_cmd_content(data->cmds);
+		if (data->cmds->next && i <= j)
+			free(data->pipes[data->cmds->index]);
+		tmp_cmd = data->cmds;
+		data->cmds = data->cmds->next;
+		free(tmp_cmd);
+		i++;
+	}
+	free(data->pipes);
+	data->pipes = NULL;
+}
+
+
+static void	pipe_failure(t_data *data, int i)
+{
+	close_failure(data->cmds, data->pipes, i);
+	// free_all(data, -2);
+
+	free(data->line);
+	data->line = NULL;
+	free_tokens(data->tokens);
+	data->tokens = NULL;
+	free_data_pipe(data, i);
+
+	free_env_exp(&data->env, &data->exp);
+	free(data);
+	data = NULL;
+	rl_clear_history();
+	exit(EXIT_FAILURE);
+}
+
 void	create_pipes(t_data *data, t_cmd *cmds)
 {
 	int	nb_pipes;
@@ -21,7 +78,7 @@ void	create_pipes(t_data *data, t_cmd *cmds)
 		if (!data->pipes[i])
 			free_all(data, EXIT_FAILURE);
 		if (pipe(data->pipes[i]) == -1)
-			free_all(data, EXIT_FAILURE);
+			pipe_failure(data, i);
 		i++;
 	}
 }
